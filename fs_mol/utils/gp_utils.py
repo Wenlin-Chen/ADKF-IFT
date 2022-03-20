605255dd2e17ac817e34bbedaa880a74169bcf98
W. Chen <wc337@gauss.eng.cam.ac.uk>
2022-03-20 16:56:45 +0000

    implemented single task Tanimoto GP as a baseline for regression and classification

--- fs_mol/utils/gp_utils.py
+++ fs_mol/utils/gp_utils.py
@@ -101,3 +101,53 @@ class VariationalGPLayer(gpytorch.models.ApproximateGP):
             
         self.train_inputs = tuple(tri.unsqueeze(-1) if tri.ndimension() == 1 else tri for tri in inputs)
         self.train_targets = targets
+
+
+def batch_tanimoto_sim(x1: torch.Tensor, x2: torch.Tensor, eps=1e-6):
+    """
+    Tanimoto between two batched tensors, across last 2 dimensions.
+    eps argument ensures numerical stability if all zero tensors are added.
+    """
+    assert x1.ndim >= 2 and x2.ndim >= 2
+    dot_prod = torch.matmul(x1, torch.transpose(x2, -1, -2))
+    x1_sum = torch.sum(x1 ** 2, dim=-1, keepdims=True)
+    x2_sum = torch.sum(x2 ** 2, dim=-1, keepdims=True)
+    return (dot_prod + eps) / (
+        eps + x1_sum + torch.transpose(x2_sum, -1, -2) - dot_prod
+    )
+
+
+class TanimotoKernel(gpytorch.kernels.Kernel):
+    """Tanimoto coefficient kernel"""
+
+    is_stationary = False
+    has_lengthscale = False
+
+    def __init__(self, **kwargs):
+        super(TanimotoKernel, self).__init__(**kwargs)
+
+    def forward(self, x1, x2, diag=False, **params):
+        if diag:
+            assert x1.size() == x2.size() and torch.equal(x1, x2)
+            return torch.ones(
+                *x1.shape[:-2], x1.shape[-2], dtype=x1.dtype, device=x1.device
+            )
+        return batch_tanimoto_sim(x1, x2)
+
+
+class ExactTanimotoGP(gpytorch.models.ExactGP):
+
+    def __init__(self, train_x, train_y, likelihood, use_numeric_labels=False):
+
+        #Set the likelihood noise and enable/disable learning
+        likelihood.noise_covar.raw_noise.requires_grad = use_numeric_labels
+        likelihood.noise_covar.noise = 0.01 if use_numeric_labels else 0.1
+        super().__init__(train_x, train_y, likelihood)
+
+        self.mean_module = gpytorch.means.ZeroMean()
+        self.covar_module = gpytorch.kernels.ScaleKernel(TanimotoKernel())
+
+    def forward(self, x):
+        mean_x = self.mean_module(x)
+        covar_x = self.covar_module(x)
+        return gpytorch.distributions.MultivariateNormal(mean_x, covar_x)
